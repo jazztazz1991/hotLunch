9a87730bb90c212f0a3ba7376f7d3f081d834e14
Cody Chase <cchase1991@gmail.com>
2017-08-01 19:59:25 -0400

    changed reserve.js

--- reserve.js
+++ reserve.js
@@ -1,6 +1,3 @@
-var currentTables = [];
-var waitingList = [];
-
 $("#add-btn").on("click", function(){
     var newReservation = {
         name: $("#name").val().trim(),
@@ -9,14 +6,6 @@ $("#add-btn").on("click", function(){
         id: $("#unique-id").val().trim()
       };
 
-    
-    
-    if(currentTables.length < 5){
-        currentTables.push(newReservation);
-    }else{
-        waitingList.push(newReservation);
-    }
-    
     $.post("/api/new", newReservation)
       .done(function(data) {
         console.log(data);
